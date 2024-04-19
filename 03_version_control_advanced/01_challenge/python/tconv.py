a92202b1c7608b4816d93a3f21ea46d5ed264384
Seiji Kamiya <10seiji18@gmail.com>
2024-04-19 14:31:44 +0900

    Add main function in tconv.py

--- 03_version_control_advanced/01_challenge/python/tconv.py
+++ 03_version_control_advanced/01_challenge/python/tconv.py
@@ -12,3 +12,14 @@ def convert_fahrenheit_to_celsius():
     fahrenheit = float(fahrenheit)
     celsius = 5/9 * (fahrenheit - 32)
     print(f"The Celsius temperature {fahrenheit} you entered is {celsius} in Fahrenheit.")
+
+def main():
+    print("Enter c if you want to convert from Fahrenheit to Celsius")
+    print("Enter f if you want to convert from Celsius to Fahrenheit")
+    user_input = input()
+    if user_input == "c":
+        convert_fahrenheit_to_celsius()
+    elif user_input == "f":
+        convert_celsius_to_fahrenheit()
+    else:
+        print("Incorrect input. Please try again later")
